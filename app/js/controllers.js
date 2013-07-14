0825295769e43b930dff987b16676ad5822f39d9
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-07-15 00:11:28 +0100

    included twitter bootstrap and some partials

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,33 @@
 'use strict';
 
-/* Controllers */
+angular.module('angular.controllers', []).
+  controller('ngAnimateCtrl', ['$scope', function($scope) {
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
+    $scope.messages = [
+      {
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        author: "Someone famous in Source Title"
+      },
+      {
+        content: "Integer molestie lorem at massa",
+        author: "Max Hoffmann"
+      },
+      {
+        content: "Nulla volutpat aliquam velit",
+        author: "Max Hoffmann"
+      },
+      {
+        content: "Aenean sit amet erat nunc",
+        author: "Max Hoffmann"
+      },
+      {
+        content: "Ac tristique libero volutpat at",
+        author: "Max Hoffmann"
+      },
+      {
+        content: "Eget porttitor lorem",
+        author: "Max Hoffmann"
+      }
+    ];
 
   }]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -3,7 +3,7 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
+angular.module('angular.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
