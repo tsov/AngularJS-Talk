07b11c7a05a7f1e7bc3ddb5134929c0d71fe5582
Max Hoffmann <maximilian.hoffmann3@mail.dcu.ie>
2013-07-17 12:54:02 +0100

    responsive website and added existing components

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,10 @@
 'use strict';
 
-angular.module('angular.controllers', []).
-  controller('ngAnimateCtrl', ['$scope', function($scope) {
+angular.module('angular.controllers', [])
+  .controller('myController', ['$scope', function($scope) {
+    // $scope.data = {message: "Whoop Whoop!"};
+  }])
+  .controller('ngAnimateCtrl', ['$scope', function($scope) {
 
     $scope.messages = [
       {
